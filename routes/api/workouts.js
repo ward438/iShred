fadd1f2db40676289019578ef6c4a7c48bdf0cd2
ward438 <75048548+ward438@users.noreply.github.com>
2021-05-24 11:44:26 -0500

    cleaned up commented code, removed unused code

--- routes/api/workouts.js
+++ routes/api/workouts.js
@@ -23,42 +23,14 @@ router.get('/range', async(req, res) => {
 router.get('/', async(req, res) => {
     Workout.find({}, function(err, items) {
         res.json(items);
-
     });
-    // try {
 
-    //     let workouts = []; // todo - get from mongo
-    //     res.json(workouts)
-    // } catch (err) {
-    //     console.log(err);
-    //     res.status(500).json(err);
-    // }
 });
 
-router.get('/:id', async(req, res) => {
-    // res.json({})
+router.get('/exercise', async(req, res) => {
     console.log("tester")
-    let workout = await Workout.findOne({ _id: req.params.id });
-    workout.exercises.push(req.body);
-    res.json(workout);
-    // find one category by its `id` value
-    // be sure to include its associated Products
-    // try {
-    //     const getAllCategories = await Category.findAll({
-    //         where: {
-    //             id: req.params.id
-    //         }
-    //     });
-    //     const categories = getAllCategories.map((product) =>
-    //         product.get({ plain: true })
-    //     );
-    //     res.json({ "categories": categories });
+})
 
-    // } catch (err) {
-    //     console.log(err);
-    //     res.status(500).json(err);
-    // }
-});
 
 
 router.put('/:id', async(req, res) => {
@@ -69,9 +41,6 @@ router.put('/:id', async(req, res) => {
     res.json(workout);
 });
 
-router.delete('/:id', (req, res) => {
-    res.json({})
-        // delete a category by its `id` value
-});
+
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,31 +1,16 @@
 const path = require('path');
 const connect = require('connect');
-// const SessionStore = require("session-mongoose")(connect);
-
 const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./routes");
-// const db = require("./models")
 const app = express();
 const PORT = process.env.PORT || 3000;
 require("dotenv").config();
-// const session = require("express-session")
-// const FileStore = require('session-file-store')(session);
 
-
-// app.use(express.session({
-//     store: store,
-//     cookie: { maxAge: 900000 } // expire session in 15 min or 900 seconds
-// }));
-
-// app.use(compression());
 app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(routes);
-
-
-
 
 mongoose.connect(process.env.MONGODB_URI, {
         useNewUrlParser: true,
@@ -34,29 +19,9 @@ mongoose.connect(process.env.MONGODB_URI, {
         useUnifiedTopology: true
     })
     .then(() => console.log('DB Connection Successful'))
-    .then(() => {
-        // const Workout = require('./models/workouts');
-        // Workout.find({}, function(err, workouts) {
-        //     console.log(workouts.length)
-        //     console.log(workouts)
-        //     console.log(workouts[0])
-
-
-        // })
-    })
-
-.catch((err) => {
-    console.error(err);
-
-
-});
-// mongoose.connection.on('error', err => {
-//     console.log('DB Connection Error; ${err.message}')
-// })
-
-// app.use(session({
-//     store: new FileStore({ path: path.join(__dirname, 'sessions') })
-// }))
+    .catch((err) => {
+        console.error(err);
+    });
 
 app.listen(PORT, function() {
     console.log(`Now listening on port: ${PORT}`);
